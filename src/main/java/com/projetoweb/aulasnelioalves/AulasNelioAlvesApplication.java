f1be42ad65c3351da73fc76998e70db4149e8b09
AlexAraujoDeBraga <alexbraga107@gmail.com>
2024-04-28 15:58:47 -0300

    Project createdd

---
+++ src/main/java/com/projetoweb/aulasnelioalves/AulasNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package com.projetoweb.aulasnelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasNelioAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasNelioAlvesApplication.class, args);
+	}
+
+}
